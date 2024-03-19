efd238a130c4f80b6a169f4d07dc31cf89b451dd
Harry Edwards <harryedwards.nc@gmail.com>
2024-03-19 23:07:02 +0000

    fix TS errors

--- src/stories/CbLayout/CbLayout.stories.ts
+++ src/stories/CbLayout/CbLayout.stories.ts
@@ -5,7 +5,10 @@ const meta = {
   title: "Layout",
   component: CbLayout,
   parameters: {
-    layout: "centered",
+    layout: "fullscreen",
+  },
+  args: {
+    children: "Hello, World!",
   },
   tags: ["autodocs"],
 } satisfies Meta<typeof CbLayout>;
@@ -14,5 +17,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    classes: "col-7",
+  },
 };
