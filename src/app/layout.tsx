c6f75fc375eb6a9cc2b1a0dca1e8bb728f1bc857
Harry Edwards <harryedwards.nc@gmail.com>
2024-03-19 16:49:49 +0000

    add base fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 
+// import "@/public/static/fonts.css";
+import "@/styles/reset.scss";
+import "@/styles/global.scss";
+
 export default function RootLayout({
   children,
 }: Readonly<{
