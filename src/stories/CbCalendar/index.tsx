e0e72e4bf10874d18f9b6676897c218ec642f05c
Harry Edwards <harryedwards.nc@gmail.com>
2025-06-03 17:22:48 +0100

    temp remove component, add bog standard embed

--- src/stories/CbCalendar/index.tsx
+++ src/stories/CbCalendar/index.tsx
@@ -1,28 +1,27 @@
 'use client';
 
 import React from 'react';
-import Calendar from '@ericz1803/react-google-calendar';
+// import Calendar from '@ericz1803/react-google-calendar';
 
 import './cb-calendar.scss';
 import { CbLayout } from '../CbLayout';
 
-const calendars = [
-  {
-    calendarId: process.env.GOOGLE_CAL_CALENDAR_ID,
-    color: '#DF302A',
-  },
-];
+// const calendars = [
+//   {
+//     calendarId: process.env.GOOGLE_CAL_CALENDAR_ID,
+//     color: '#DF302A',
+//   },
+// ];
 
-interface Props {}
-
-export const CbCalendar = ({ ...props }: Props) => {
+export const CbCalendar = () => {
   return (
     <CbLayout container classes="col-sm-12">
       <div className="calendar">
-        <Calendar
+        {/* <Calendar
           apiKey={process.env.GOOGLE_CAL_API_KEY}
           calendars={calendars}
-        />
+        /> */}
+        <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Europe%2FLondon&showPrint=0&showTitle=0&showTabs=0&showCalendars=0&showTz=0&src=Y19hZmY0ODdiOWFlZDVjMjMwMjI0MDUzMGFlMDJiMTkxZDdkZDFmNDU0ZGVhNjRlMDNjMDNlODUyMTQzNzhiMjFhQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039be5"></iframe>
       </div>
     </CbLayout>
   );
